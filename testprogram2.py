c7d2f45387741b7b12d0674a9666b137360bb688
Prasid Dasgupta <prasid.dasgupt@yahoo.com>
2020-07-08 21:37:07 -0400

    first torch statement run successfully

--- testprogram2.py
+++ testprogram2.py
@@ -1,3 +1,9 @@
+from __future__ import print_function
+import torch
+
 print("Hello world from untitled")
 print("now change the hello world untitlles program")
 print("try try try again")
+
+x = torch.rand(5, 3)
+print(x)
